74cc047e7191d551ad1208578ef470832b2e11da
coffmanjrp <coffmanjrp@gmail.com>
2021-02-06 23:41:54 -0800

    fetching data when page loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Navbar } from './components/layout';
 import { Users } from './components/users';
 import './App.css';
 
 function App() {
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      login: 'mojombo',
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    },
-    {
-      id: 2,
-      login: 'defunkt',
-      avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-      html_url: 'https://github.com/defunkt',
-    },
-    {
-      id: 3,
-      login: 'pjhyett',
-      avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-      html_url: 'https://github.com/pjhyett',
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const fetchData = async () => {
+      const res = await axios.get('https://api.github.com/users');
+      setUsers(res.data);
+      setLoading(false);
+    };
+
+    try {
+      fetchData();
+    } catch (error) {
+      throw new Error(error);
+    }
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <div>
       <Navbar />
       <div className="container">
-        <Users users={users} />
+        <Users users={users} loading={loading} />
       </div>
     </div>
   );
