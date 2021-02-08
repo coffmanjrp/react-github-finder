2066befabdcbc10dca50dc49da76a28372ef66d6
coffmanjrp <coffmanjrp@gmail.com>
2021-02-07 20:04:32 -0800

    implement alert function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Navbar } from './components/layout';
+import { Alert, Navbar } from './components/layout';
 import { Search, Users } from './components/users';
 import './App.css';
 
 function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
 
   const clientID = process.env.REACT_APP_GITHUB_CLIENT_ID;
   const clientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
@@ -40,6 +41,7 @@ function App() {
         `https://api.github.com/search/users?q=${text}&client_id=${clientID}&client_secret=${clientSecret}`
       );
       setUsers(res.data.items);
+      setAlert(null);
       setLoading(false);
     };
 
@@ -50,11 +52,32 @@ function App() {
     }
   };
 
+  // Clear users from state
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
+  };
+
+  // Set alert
+  const callAlert = (msg, type) => {
+    setAlert({ msg, type });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 5000);
+  };
+
   return (
     <div>
       <Navbar />
       <div className="container">
-        <Search searchUsers={searchUsers} />
+        <Alert alert={alert} />
+        <Search
+          clearUsers={clearUsers}
+          callAlert={callAlert}
+          searchUsers={searchUsers}
+          showClear={users.length > 0 ? true : false}
+        />
         <Users users={users} loading={loading} />
       </div>
     </div>
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Search = ({ searchUsers }) => {
+const Search = ({ clearUsers, searchUsers, callAlert, showClear }) => {
   const [text, setText] = useState('');
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    searchUsers(text);
-    setText('');
+    if (text === '') {
+      callAlert('Please enter something', 'light');
+    } else {
+      searchUsers(text);
+      setText('');
+    }
   };
 
   const onChange = (e) => {
@@ -31,12 +35,20 @@ const Search = ({ searchUsers }) => {
           className="btn btn-dark btn-block"
         />
       </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
     </div>
   );
 };
 
 Search.propTypes = {
+  clearUsers: PropTypes.func.isRequired,
   searchUsers: PropTypes.func.isRequired,
+  callAlert: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
 };
 
 export default Search;
