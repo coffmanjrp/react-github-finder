26ae78d9b2babffc3c0cec98460fccfa5bd92352
coffmanjrp <coffmanjrp@gmail.com>
2021-02-07 22:07:58 -0800

    implement user data to Uesr component

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,15 +1,19 @@
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Spinner } from '../layout';
+import PropTypes from 'prop-types';
 
-const User = ({ getUser, loading, match, user }) => {
+const User = ({ getUser, match, loading, user }) => {
   const {
     name,
     avatar_url,
     location,
     bio,
     blog,
+    company,
     login,
     html_url,
-    follwers,
+    followers,
     following,
     public_repos,
     public_gists,
@@ -21,7 +25,81 @@ const User = ({ getUser, loading, match, user }) => {
     // eslint-disable-next-line
   }, []);
 
-  return <div>{name}</div>;
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <Link to="/" className="btn btn-light">
+        Back To Search
+      </Link>
+      Hireable:{' '}
+      {hireable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            alt={login}
+            className="round-img"
+            style={{ width: '150px' }}
+          />
+          <h1>{login}</h1>
+          {location && <p>Location: {location}</p>}
+        </div>
+        <div>
+          {bio && (
+            <>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit GitHub Profile
+          </a>
+          <ul>
+            <li>
+              {name && (
+                <>
+                  <strong>Name: </strong> {name}
+                </>
+              )}
+            </li>
+            <li>
+              {company && (
+                <>
+                  <strong>Company: </strong> {company}
+                </>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <>
+                  <strong>Website: </strong> {blog}
+                </>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+    </>
+  );
+};
+
+User.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+  getUser: PropTypes.func.isRequired,
 };
 
 export default User;
